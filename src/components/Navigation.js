22ee5a2fc2029ab6f9ffe24a1ac3a6bb17b46826
mangomateo <velarde.matthew@gmail.com>
2021-10-29 11:27:01 -0700

    created nav component

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+const Navigation = () => {
+    return (
+        <nav>
+            {/* STARBUCKS LOGO GOES HERE */}
+            <img src="#" alt="#" />
+            <ul>
+                <li>
+                { /* eslint-disable-next-line */ }
+                <a href="#">MENU</a>
+                </li>
+                <li>
+                { /* eslint-disable-next-line */ }
+                <a href="#">REWARDS</a>
+                </li>
+                <li>
+                { /* eslint-disable-next-line */ }
+                <a href="#">GIFT CARDS</a>
+                </li>
+            </ul>
+            <div id="buttons">
+                { /* eslint-disable-next-line */ }
+                <a href="#">Find a Store</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation;
